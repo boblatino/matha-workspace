983d8f769394919182d1bdf5cd356c9c530f60c2
ghost5egy <ghost5egy@a977719c-7810-fd26-1f69-4f24e71d21f2>
2011-01-22 04:59:10 +0000

    mwsConfigReader

--- mwsConfigReader.cpp
+++ mwsConfigReader.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <string>
+#include <string.h>
 #include <map>
 #include "mwsConfigReader.h"
 
--- mwsConfigReader.h
+++ mwsConfigReader.h
@@ -1,25 +1,16 @@
 #ifndef __mwsConfigReader__
 #define __mwsConfigReader__
 
-#include <string>
-#include <map>
+class mwsConfigReader {
+   private:
+     std::map<std::string ,std::map<std::string , std::string > > data;
+     std::string filename;
+     bool readconf(std::string ,std::map<std::string ,std::map<std::string , std::string > > &);
+   public :
+     mwsConfigReader(std::string filen);  
+     std::string getAttrib(std::string scope,std::string attrib);
+     std::map<std::string ,std::string> getScope(std::string scope);
+     std::map<std::string ,std::map<std::string , std::string > > getAll(); 
+};
 
-using namespace std;
-
-namespace mws
-{
-
-	class mwsConfigReader {
-	   private:
-	     std::map<std::string ,std::map<std::string , std::string > > data;
-	     std::string filename;
-	     bool readconf(std::string ,std::map<std::string ,std::map<std::string , std::string > > &);
-	   public :
-	     mwsConfigReader(std::string filen);  
-	     std::string getAttrib(std::string scope,std::string attrib);
-	     std::map<std::string ,std::string> getScope(std::string scope);
-	     std::map<std::string ,std::map<std::string , std::string > > getAll(); 
-	};
-
-}
 #endif
