fb8065f71466aebd3e92e6d5458acc57ce9ceebe
michael.behman@gmail.com <michael.behman@gmail.com@a977719c-7810-fd26-1f69-4f24e71d21f2>
2011-08-23 09:07:34 +0000

    irfft.

--- trunk/docs/audio/SRC/voicebox.cpp
+++ trunk/docs/audio/SRC/voicebox.cpp
@@ -84,154 +84,117 @@ Matrix rfft( Matrix x, size_t n, size_t d)
 
 Matrix irfft( Matrix y )
 {
-/*
-function x=irfft(y,n,d)
+	size_t col, row, d;
 
-s=size(y);
-ps=prod(s);
-ns=length(s);
-if ps==1
-    x=y
-else
-    if nargin <3 || isempty(d)
-        d=find(s>1);
-        d=d(1);
-    end
-    m=s(d);
-    k=ps/m;     % number of fft's to do
-    if d==1
-        v=reshape(y,m,k);
-    else
-        v=reshape(permute(y,[d:ns 1:d-1]),m,k);
-    end
-    if nargin<2 || isempty(n)
-        n=2*m-2;        % default output length
-    else
-        mm=1+fix(n/2);          % expected input length
-        if mm>m v=[v; zeros(mm-m,k)];   % zero pad
-        elseif mm<m v(mm+1:m,:)=[];     % or truncate
-        end
-        m=mm;
-    end
-    if rem(n,2)		% odd output length
-        x=real(ifft([v;conj(v(m:-1:2,:))],[],1));    % do it the long way
-    else			% even output length
-        v(m,:)=real(v(m,:));	% force nyquist element real
-        w=ones(1,k);
-        %  t=[cumprod([-0.5i; exp(2i*pi/n)*ones(m-2,1)]); 0.5i];
-        t=-0.5i* exp((2i*pi/n)*(0:m-1)).';
-        z=(t(:,w)+0.5).*(conj(flipud(v))-v)+v;
-        z(m,:)=[];
-        zz=ifft(z,[],1);
-        x=zeros(n,k);
-        x(1:2:n,:)=real(zz);
-        x(2:2:n,:)=imag(zz);
-    end
-    s(d)=n;         % change output dimension
-    if d==1
-        x=reshape(x,s);
-    else
-        x=permute(reshape(x,s([d:ns 1:d-1])),[ns+2-d:ns 1:ns+1-d]);
-    end
-end
+	y.size( &col ,&row );
+	size_t ps = col * row;
 
-*/
+	if( length( y ) == 1 )
+		return y;
+
+	if( row > 1 )
+		d = 1;
+	else
+		d = 2;
+
+	size_t m = d == 1 ? row : col;
+	size_t k = ps / m;
+
+	Matrix v, x;
+	if( d == 1 )
+		v = y;
+	else
+		v = y.transpose();
+
+	size_t n = 2 * m - 2;        // default output length
+
+	if( n % 2 )		// odd output length
+	{
+		Matrix temp = v, temp2 = conj( v );
+		complex <double> dat[ col ];
+		size_t l, ll;
+		v.size( &l, &ll );
+		for( size_t i = m; i >= 2; i-- )
+		{
+			for( size_t j = 0; j < l; j++ )
+				temp2.getElement( dat + j, j, i - 1  );
+			temp.addRow( dat, l );
+		}
+		x = real( ifft( temp ) );    // do it the long way
+	}
+	else			// even output length
+	{
+		complex <double> dat[ k ], dat2[ k ];
+		for( size_t i = 0; i < col; i++ )
+		{
+			v.getElement( dat, i, m - 1 );
+			v.setElement( real( *dat ), i, m - 1 );
+		}
+		Matrix t = exp( seq( 0, m - 1 ) * complex<double> ( 0, 2 * M_PI / n ) ).transpose() * complex <double>( 0, -0.5 );
+		t = repmat( t, 1, k );
+		Matrix z = dotProduct( ( t + 0.5 ), ( conj( flipud( v ) ) - v ) ) + v;
+		z.delRow( m - 1 );
+		z = ifft( z );
+		Matrix zr = real( z );
+		Matrix zi = imag( z );
+		for( size_t i = 0; i < n / 2; i++ )
+		{
+			for( size_t j = 0; j < k; j++ )
+			{
+				zr.getElement( dat + j, j, i );
+				zi.getElement( dat2 + j, j, i );
+			}
+			x.addRow( dat, k );
+			x.addRow( dat2, k );
+		}
+	}
+
+	if( d == 2 )
+		x = x.transpose();
+
+	return x;
 }
 
 
 Matrix irfft( Matrix y, size_t n )
 {
-/*
-function x=irfft(y,n,d)
 
-s=size(y);
-ps=prod(s);
-ns=length(s);
-if ps==1
-    x=y
-else
-    if nargin <3 || isempty(d)
-        d=find(s>1);
-        d=d(1);
-    end
-    m=s(d);
-    k=ps/m;     % number of fft's to do
-    if d==1
-        v=reshape(y,m,k);
-    else
-        v=reshape(permute(y,[d:ns 1:d-1]),m,k);
-    end
-    if nargin<2 || isempty(n)
-        n=2*m-2;        % default output length
-    else
-        mm=1+fix(n/2);          % expected input length
-        if mm>m v=[v; zeros(mm-m,k)];   % zero pad
-        elseif mm<m v(mm+1:m,:)=[];     % or truncate
-        end
-        m=mm;
-    end
-    if rem(n,2)		% odd output length
-        x=real(ifft([v;conj(v(m:-1:2,:))],[],1));    % do it the long way
-    else			% even output length
-        v(m,:)=real(v(m,:));	% force nyquist element real
-        w=ones(1,k);
-        %  t=[cumprod([-0.5i; exp(2i*pi/n)*ones(m-2,1)]); 0.5i];
-        t=-0.5i* exp((2i*pi/n)*(0:m-1)).';
-        z=(t(:,w)+0.5).*(conj(flipud(v))-v)+v;
-        z(m,:)=[];
-        zz=ifft(z,[],1);
-        x=zeros(n,k);
-        x(1:2:n,:)=real(zz);
-        x(2:2:n,:)=imag(zz);
-    end
-    s(d)=n;         % change output dimension
-    if d==1
-        x=reshape(x,s);
-    else
-        x=permute(reshape(x,s([d:ns 1:d-1])),[ns+2-d:ns 1:ns+1-d]);
-    end
-end
-
-*/
-}
-
-
-Matrix irfft( Matrix y, size_t n, size_t d )
-{
-
-	size_t col, row;
+	size_t col, row, d;
 
 	y.size( &col ,&row );
 	size_t ps = col * row;
-	size_t ns = 2; //length( s );
 
 	if( length( y ) == 1 )
 		return y;
 
+	if( row > 1 )
+		d = 1;
+	else
+		d = 2;
+
 	size_t m = d == 1 ? row : col;
-	size_t k = ps / m;     // number of fft's to do
+	size_t k = ps / m;
 
 	Matrix v, x;
 	if( d == 1 )
-		v = y;	//v=reshape(y,m,k);
+		v = y;
 	else
-		v = y.transpose();	//v=reshape(permute(y,[d:ns 1:d-1]),m,k);
+		v = y.transpose();
 
 //	if nargin<2 || isempty(n)
 //		n=2*m-2;        // default output length
 //	else
-		size_t mm = 1 + fix( n / 2 );          // expected input length
-		if( mm > m )
-		{
-			double zer[ col ];
-			bzero( zer, col * sizeof( double ) );
-			//v = [v; zeros(mm-m,k)];   // zero pad
-			for( size_t i = m; i <= mm; i++ )
-				v.addRow( zer, col );
-		}
-		else if( mm < m )
-			v = v.getSubMatrix( 0, col - 1, 0, mm - 1 );	//v(mm+1:m,:)=[];     // or truncate
-		m = mm;
+	size_t mm = 1 + fix( n / 2 );          // expected input length
+	if( mm > m )
+	{
+		double zer[ col ];
+		bzero( zer, col * sizeof( double ) );
+		for( size_t i = m; i <= mm; i++ )
+			v.addRow( zer, col );
+	}
+	else if( mm < m )
+		v = v.getSubMatrix( 0, col - 1, 0, mm - 1 );
+	m = mm;
 //	end
 
 	if( n % 2 )		// odd output length
@@ -250,20 +213,17 @@ Matrix irfft( Matrix y, size_t n, size_t d )
 	}
 	else			// even output length
 	{
-		//v(m,:)=real(v(m,:));	// force nyquist element real
 		complex <double> dat[ k ], dat2[ k ];
 		for( size_t i = 0; i < col; i++ )
 		{
 			v.getElement( dat, i, m - 1 );
 			v.setElement( real( *dat ), i, m - 1 );
 		}
-//		Matrix w = ones( 1, k );
 		Matrix t = exp( seq( 0, m - 1 ) * complex<double> ( 0, 2 * M_PI / n ) ).transpose() * complex <double>( 0, -0.5 );
 		t = repmat( t, 1, k );
 		Matrix z = dotProduct( ( t + 0.5 ), ( conj( flipud( v ) ) - v ) ) + v;
-		z.delRow( m - 1 );	//z(m,:)=[];
+		z.delRow( m - 1 );
 		z = ifft( z );
-//		x = zeros( n, k );
 		Matrix zr = real( z );
 		Matrix zi = imag( z );
 		for( size_t i = 0; i < n / 2; i++ )
@@ -276,20 +236,88 @@ Matrix irfft( Matrix y, size_t n, size_t d )
 			x.addRow( dat, k );
 			x.addRow( dat2, k );
 		}
-/*		x(1:2:n,:) = real( z );
-		x(2:2:n,:) = imag( z );
-*/
 	}
 
-/*	if( d == 1 )
-	{
-		x = reshape( x, n, col);
-	}
+	if( d == 2 )
+		x = x.transpose();
+
+	return x;
+}
+
+
+Matrix irfft( Matrix y, size_t n, size_t d )
+{
+
+	size_t col, row;
+
+	y.size( &col ,&row );
+	size_t ps = col * row;
+
+	if( length( y ) == 1 )
+		return y;
+
+	size_t m = d == 1 ? row : col;
+	size_t k = ps / m;
+
+	Matrix v, x;
+	if( d == 1 )
+		v = y;
 	else
+		v = y.transpose();
+
+	size_t mm = 1 + fix( n / 2 );          // expected input length
+	if( mm > m )
 	{
-		x = reshape( x, n, col ).transpose();;
+		double zer[ col ];
+		bzero( zer, col * sizeof( double ) );
+		for( size_t i = m; i <= mm; i++ )
+			v.addRow( zer, col );
 	}
-*/	
+	else if( mm < m )
+		v = v.getSubMatrix( 0, col - 1, 0, mm - 1 );
+	m = mm;
+
+	if( n % 2 )		// odd output length
+	{
+		Matrix temp = v, temp2 = conj( v );
+		complex <double> dat[ col ];
+		size_t l, ll;
+		v.size( &l, &ll );
+		for( size_t i = m; i >= 2; i-- )
+		{
+			for( size_t j = 0; j < l; j++ )
+				temp2.getElement( dat + j, j, i - 1  );
+			temp.addRow( dat, l );
+		}
+		x = real( ifft( temp ) );    // do it the long way
+	}
+	else			// even output length
+	{
+		complex <double> dat[ k ], dat2[ k ];
+		for( size_t i = 0; i < col; i++ )
+		{
+			v.getElement( dat, i, m - 1 );
+			v.setElement( real( *dat ), i, m - 1 );
+		}
+		Matrix t = exp( seq( 0, m - 1 ) * complex<double> ( 0, 2 * M_PI / n ) ).transpose() * complex <double>( 0, -0.5 );
+		t = repmat( t, 1, k );
+		Matrix z = dotProduct( ( t + 0.5 ), ( conj( flipud( v ) ) - v ) ) + v;
+		z.delRow( m - 1 );
+		z = ifft( z );
+		Matrix zr = real( z );
+		Matrix zi = imag( z );
+		for( size_t i = 0; i < n / 2; i++ )
+		{
+			for( size_t j = 0; j < k; j++ )
+			{
+				zr.getElement( dat + j, j, i );
+				zi.getElement( dat2 + j, j, i );
+			}
+			x.addRow( dat, k );
+			x.addRow( dat2, k );
+		}
+	}
+
 	if( d == 2 )
 		x = x.transpose();
 
